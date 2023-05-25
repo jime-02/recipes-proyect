ef24e689af5fdb47ff3cdf2885262097a4bfff02
jime-02 <jimenamjse02@gmail.com>
2023-05-24 23:07:09 -0600

    Componentes de los cards de Mejores recetas y Nuevas recetas

---
+++ dist/js/components/BestRecipeCard.js
@@ -0,0 +1,44 @@
+app.component('best-recipe-card', {
+
+    props:{
+        image:{
+            type: String
+        },
+
+        category: {
+            type: String,
+            default: "Default category"
+        },
+
+        name: {
+            type: String,
+            default: "Default name"
+        },
+
+        level: {
+            type: String,
+            default: "Default level"
+        },
+ 
+    },
+
+    methods: {
+       
+    },
+
+    template: 
+
+    /*html*/
+    ` 
+    <div class="card p-3 shadow border-0 d-inline-block card-color mb-4">
+        <img v-bind:src="image" class="card-img-top img-height rounded mb-4" alt="featured recipe">
+        <div class="card-body p-0">
+            <p class="mt-2 mb-3 category-text">{{ category }}</p>
+            <h5 class="mb-3 card-title overflow-hidden text-recipe-name">{{ name }}</h5>
+            <p class="mb-3 text-recipe-level">{{ level }}</p>
+
+            <button class="btn-view mt-2 mb-2 align-middle" v-on:click="onClickViewRecipe(index)" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Ver receta</button>
+        </div>
+    </div> 
+  ` 
+})
